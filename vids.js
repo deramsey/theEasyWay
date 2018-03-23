88ec10a80a4980ebb4dbb7bd2dbd32f156638d3b
Derek <d@deramcreative.com>
2018-03-23 15:42:56 -0400

    Big Updates

---
+++ vids.js
@@ -0,0 +1,91 @@
+var videos = {
+p1: {title: "Dr.", fname: "Bob", lname: "Appleman", url: "TipGTRlJZg4", page: "", thumbid: "v1521827768", thumbname: "ApplemanBob.png", init: "1"},
+p2: {title: "Dr.", fname: "Floyd", lname: "Brooker", url: "", page: "", thumbid: "v1521818839", thumbname:"audioonly.png", init: "1"},
+p3: {title: "Dr.", fname: "James", lname: "Brown", url: "", page: "", thumbid: "v1521818839", thumbname:"audioonly.png", init: "1"},
+p4: {title: "Dr.", fname: "Bill", lname: "Burns", url: "", page: "", thumbid: "v1521830227", thumbname: "BurnsBill.png", init: "1"},
+p5: {title: "Dr.", fname: "John", lname: "Burton", url: "", page: "", thumbid: "v1521830227", thumbname: "BurtonJohn.png", init: "1"},
+p6: {title: "Dr.", fname: "Rebecca", lname: "Butler", url: "", page: "", thumbid: "v1521829053", thumbname: "ButlerRebecca.png", init: "1"},
+p7: {title: "Dr.", fname: "Edward", lname: "Cafarella", url: "", page: "", thumbid: "v1521829053", thumbname: "CafarellaEdward.png", init: "1"},
+p8: {title: "Dr.", fname: "Ward", lname: "Cates", url: "", page: "", thumbid: "v1521830227", thumbname: "CatesWard.png", init: "1"},
+p9: {title: "Dr.", fname: "Richard", lname: "Clark", url: "", page: "", thumbid: "v1521830227", thumbname: "ClarkRichard.png", init: "1"},
+p10: {title: "Dr.", fname: "Richard", lname: "Cornell", url: "", page: "", thumbid: "v1521830227", thumbname: "CornellRichard.png", init: "1"},
+p11: {title: "Dr.", fname: "Edgar", lname: "Dale", url: "", page: "", thumbid: "v1521818839", thumbname:"audioonly.png", init: "1"},
+p12: {title: "Dr.", fname: "Ivor", lname: "Davies", url: "", page: "", thumbid: "v1521830227", thumbname: "DaviesIvor.png", init: "1"},
+p13: {title: "Dr.", fname: "Marcy", lname: "Driscoll", url: "", page: "", thumbid: "v1521830227", thumbname: "DriscollMarcy.png", init: "1"},
+p14: {title: "Dr.", fname: "Donald", lname: "Ely", url: "", page: "", thumbid: "v1521830228", thumbname: "ElyDonald.png", init: "1"},
+p15: {title: "Dr.", fname: "Malcom", lname: "Fleming", url: "", page: "", thumbid: "v1521058736", thumbname:"FlemingMalcolm.png", init: "1"},
+p16: {title: "Dr.", fname: "Louis", lname: "Forsdale", url: "", page: "", thumbid: "v1521818839", thumbname:"audioonly.png", init: "1"},
+p17: {title: "Dr.", fname: "Robert", lname: "Gagne", url: "", page: "", thumbid: "v1521830227", thumbname: "GagneRobert.png", init: "1"},
+p18: {title: "Dr.", fname: "Andrew", lname: "Gibbons", url: "", page: "", thumbid: "v1521830227", thumbname: "GibbonsAndy.png", init: "1"},
+p19: {title: "", fname: "Elizabeth", lname: "Golterman", url: "", page: "", thumbid: "v1521818839", thumbname:"audioonly.png", init: "1"},
+p20: {title: "Dr.", fname: "Mike", lname: "Hannafin", url: "", page: "", thumbid: "v1521830227", thumbname: "HannafinMike.png", init: "1"},
+p21: {title: "Dr.", fname: "Robert", lname: "Harrell", url: "", page: "", thumbid: "v1521830227", thumbname: "HarrellRobert.png", init: "1"},
+p22: {title: "Dr.", fname: "Jay", lname: "Harriman", url: "", page: "", thumbid: "v1521830228", thumbname: "HarrimanJay.png", init: "1"},
+p23: {title: "Dr.", fname: "Phil", lname: "Harris", url: "", page: "", thumbid: "v1521830228", thumbname: "HarrisPhil.png", init: "1"},
+p24: {title: "Dr.", fname: "Robert", lname: "Heinich", url: "", page: "", thumbid: "v1521830228", thumbname: "HeinichRobert.png", init: "1"},
+p25: {title: "Dr.", fname: "Mary", lname: "Herring", url: "", page: "", thumbid: "v1521058736", thumbname:"HerringMary.png", init: "1"},
+p26: {title: "Dr.", fname: "Dave", lname: "Jonassen", url: "", page: "", thumbid: "v1521830228", thumbname: "JonassenDave.png", init: "2"},
+p27: {title: "Dr.", fname: "David", lname: "Jonassen", url: "", page: "", thumbid: "v1521830228", thumbname: "JonassenDavid.png", init: "2"},
+p28: {title: "Dr.", fname: "Roger", lname: "Kaufman", url: "", page: "", thumbid: "v1521830228", thumbname: "KaufmanRoger.png", init: "2"},
+p29: {title: "Dr.", fname: "Jerrold", lname: "Kemp", url: "", page: "", thumbid: "v1521830228", thumbname: "KempJerrold.png", init: "2"},
+p30: {title: "Dr.", fname: "Addie", lname: "Kinsinger", url: "", page: "", thumbid: "v1521830228", thumbname: "KinsingerAddie.png", init: "2"},
+p31: {title: "Dr.", fname: "Donald", lname: "Kirkpatrick", url: "", page: "", thumbid: "v1521830227", thumbname: "KirkpatrickDonald.png", init: "2"},
+p32: {title: "Dr.", fname: "James", lname: "Klein", url: "", page: "", thumbid: "v1521831556", thumbname: "KleinJames.png", init: "2"},
+p33: {title: "Dr.", fname: "Richard", lname: "Mayer", url: "", page: "", thumbid: "v1521058736", thumbname:"MayerRichard.png", init: "2"},
+p34: {title: "Dr.", fname: "F. Dean", lname: "McClusky", url: "", page: "", thumbid: "v1521818839", thumbname:"audioonly.png", init: "2"},
+p35: {title: "Dr.", fname: "Marina", lname: "McIsaac", url: "", page: "", thumbid: "v1521058736", thumbname:"McIsaacMarina.png", init: "2"},
+p36: {title: "Dr.", fname: "David", lname: "Merrill", url: "", page: "", thumbid: "v1521831556", thumbname: "MerrillDavid.png", init: "2"},
+p37: {title: "Dr.", fname: "Al", lname: "Mizell", url: "", page: "mizellal.html", thumbid: "v1521831556", thumbname: "MizellAl.png", init: "2"},
+p38: {title: "Dr.", fname: "Mike", lname: "Molenda", url: "", page: "molendamike.html", thumbid: "v1521831556", thumbname: "MolendaMike.png", init: "2"},
+p39: {title: "Dr.", fname: "Mike", lname: "Moore", url: "", page: "mooremike.html", thumbid: "v1521832584", thumbname: "MooreMike.png", init: "2"},
+p40: {title: "Dr.", fname: "Roy", lname: "Morgan", url: "", page: "morganroy.html", thumbid: "v1521831556", thumbname: "MorganRoy.png", init: "2"},
+p41: {title: "Dr.", fname: "Kyle", lname: "Peck", url: "", page: "peckkyle.html", thumbid: "v1521831556", thumbname: "PackKyle.png", init: "3"},
+p42: {title: "Dr.", fname: "Kay", lname: "Persichitte", url: "", page: "persichittekay.html", thumbid: "v1521831556", thumbname: "PersichitteKay.png", init: "3"},
+p43: {title: "Dr.", fname: "Thomas", lname: "Reeves", url: "", page: "reevesthomas.html", thumbid: "v1521831556", thumbname: "ReevesThomas.png", init: "3"},
+p44: {title: "Dr.", fname: "Robert", lname: "Reiser", url: "", page: "reiserrobert.html", thumbid: "v1521831556", thumbname: "ReiserRobert.png", init: "3"},
+p45: {title: "Dr.", fname: "Rita", lname: "Richey", url: "", page: "richeyrita.html", thumbid: "v1521831556", thumbname: "RicheyRita.png", init: "3"},
+p46: {title: "Dr.", fname: "Mendel", lname: "Sherman", url: "", page: "shermanmendel.html", thumbid: "v1521831556", thumbname: "ShermanMendel.png", init: "3"},
+p47: {title: "Dr.", fname: "Sharon", lname: "Smaldino", url: "", page: "smaldinosharon.html", thumbid: "v1521831557", thumbname: "SmaldinoSharon.png", init: "3"},
+p48: {title: "Dr.", fname: "Mike", lname: "Spector", url: "", page: "spectormike.html", thumbid: "v1521831557", thumbname: "SpectorMike.png", init: "3"},
+p49: {title: "Dr.", fname: "Atwi", lname: "Suparman", url: "", page: "suparmanatwi.html", thumbid: "v1521832584", thumbname: "SuparmanAtwi.png", init: "3"},
+p50: {title: "Dr.", fname: "I. Keith", lname: "Tyler", url: "", page: "tylerkeith.html", thumbid: "v1521818839", thumbname:"audioonly.png", init: "3"},
+p51: {title: "Dr.", fname: "Hans", lname: "Wennberg", url: "", page: "wennberghans.html", thumbid: "v1521831557", thumbname: "WennbergHans.png", init: "4"},
+p52: {title: "Dr.", fname: "Charles", lname: "White", url: "", page: "whitecharles.html", thumbid: "v1521831557", thumbname: "WhiteCharles.png", init: "4"},
+p53: {title: "Dr.", fname: "Walt", lname: "Wittich", url: "", page: "wittichwalt.html", thumbid: "v1521831556", thumbname: "WittichWalt.png", init: "4"},
+p54: {title: "Dr.", fname: "Leo", lname: "Yam", url: "aynB3Epv6Iw", page: "yamleo.html", thumbid: "v1521058736", thumbname:"YamLeo.png", init: "4"}
+
+};
+
+$(document).ready(function(){
+	$.each(videos, function(i, value){
+		$("#vidContainer").append("<div class='p-2 vidImg  "+videos[i]["init"]+"' style='background-image: url(\"http:/res.cloudinary.com/dwuwuaov5/image/upload/" + videos[i]["thumbid"] +"/" +  videos[i]["thumbname"] + "\"); background-size: cover; padding: 0px !important'><div class='layer'></div><h3 class='name' style='color: white; margin-top: 90px; text-shadow: 0 2px 4px black;'>" + videos[i]["title"] + " " + videos[i]["fname"] + " " + videos[i]["lname"] + "</h3></div>");
+
+	});
+
+
+
+		$("input[name=last_name]").on('click', function(){
+		var radVal = $("input[name=last_name]:checked").val();
+		if (radVal==='atog'){
+			$(".2, .3, .4").hide();
+			$(".1").show();
+		}
+		else if (radVal==='hton'){
+			$(".1, .3, .4").hide();
+			$(".2").show();
+		}
+		else if (radVal==='otou'){
+			$(".1, .2, .4").hide();
+			$(".3").show();
+		}
+		else if (radVal==='vtoz'){
+			$(".1, .3, .2").hide();
+			$(".4").show();
+		}
+		else{
+			$(".1, .2, .3, .4").show();
+		}
+	});
+
+
+});
